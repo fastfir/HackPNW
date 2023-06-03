419e0639f518d42c088c8753dd7596bc3bda62d6
fastfir <95661133+fastfir@users.noreply.github.com>
2023-06-03 12:58:08 -0700

    Moved to frontend

--- graphics.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('webgl');
-    canvas.width = 800;
-    canvas.height = 800;
-});
