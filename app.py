df2a67fd39f2dd8d5094bdcf982f870908e58e0c
fastfir <95661133+fastfir@users.noreply.github.com>
2023-06-03 14:43:23 -0700

    Changed folders

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
+import random
 import logic
 app = Flask(__name__)
 game = logic.Game()
 @app.route("/")
-def game():
-    return "Hello World!"
+def home():
+    return render_template("index.html")
 @app.route("/game", methods=['POST'])
 def backend():
-    game.evan(request.form['day'],request.form['money'],request.form['size'])
+    game.log(request.form['day'],request.form['money'],request.form['size'])
+@app.route("/question")
+def question():
+    return game.questions[random.range(0,len(game.questions))]
