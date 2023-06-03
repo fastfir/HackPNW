f66f02dbf99863b1ef5c16427f32c327137aa030
roar-k <124755801+roar-k@users.noreply.github.com>
2023-06-03 13:50:34 -0700

    graphics

--- frontend/graphics.js
+++ frontend/graphics.js
@@ -1,6 +1,23 @@
 window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('webgl');
+    const ctx = canvas.getContext('2d');
     canvas.width = 800;
     canvas.height = 800;
+    ctx.fillStyle = "#34dbeb";
+    ctx.fillRect(400, 50, 400, 150);
+
+    ctx.fillStyle = "black";
+    ctx.font = "48px sans-serif";
+    ctx.fillText("Protesting Game", 420, 140);
+
+    const img = new Image();
+    img.addEventListener(
+  	"load",
+  () => {
+    // execute drawImage statements here
+  },
+  false
+);
+img.src = "menu button.svg"; // Set source path
+
 });
