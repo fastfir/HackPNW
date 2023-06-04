bd12e1886a2b91c08fad9ef71873a5afad35a474
fastfir <95661133+fastfir@users.noreply.github.com>
2023-06-04 09:49:17 -0700

    Added JSON

--- logic.py
+++ logic.py
@@ -5,9 +5,11 @@ class Game:
         self.money = 0
         self.protestSize = 1
         self.rep = 0
+        self.items = ["Bigger Sign",10,"Megaphone",15,"Leaflets",20]
     def log(self,day,money,size):
         self.day = day
         self.money = money
         self.protestSize = size
     def calcRep(self, score, size, items):
         self.rep += score + int(size / 5)
+    
