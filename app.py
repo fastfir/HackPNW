ab299a94b1e3cf3d72d292b26b99e5e30f51ff1e
fastfir <95661133+fastfir@users.noreply.github.com>
2023-06-04 09:49:30 -0700

    JSON

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template, request
 import random
+import json
 import logic
 app = Flask(__name__)
 game = logic.Game()
 @app.route("/")
 def home():
-    return render_template("index.html")
-@app.route("/game", methods=['POST'])
-def backend():
-    game.log(request.form['day'],request.form['money'],request.form['size'])
+    return render_template("game menu.html")
+@app.route("/city")
+def city():
+    return render_template("city.html")
+@app.route("/log", methods=['POST'])
+def log():
+    game.log(request.form['day'], request.form['money'], request.form['size'])
 @app.route("/question")
 def question():
-    return game.questions[random.range(0,len(game.questions))]
+    question = game.questions[random.randrange(len(game.questions))]
+    return {
+        "question": question
+    }
+@app.route("/shop")
+def shop():
+    itemList = []
+    for item in game.items:
+        itemList.append(item)
+    return {
+
+    }
